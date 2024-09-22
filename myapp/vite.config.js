4cfb63381d8902714fca71377550681274bcee5a
Mrinal Anand <mrinal.annand@gmail.com>
2024-09-22 21:19:11 +0530

    adding front-end repo name for deploying on github

--- myapp/vite.config.js
+++ myapp/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // adjust according to your backend port
+      '/api': 'http://localhost:5000', // Back-end port
+      'base': '/Teaching_App_Front_End' // Front-end repo name for deployment on github
     },
   },
 });
